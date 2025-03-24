54dec2afb7eba401b4bf25e395b81baf01814558
Aman Kumar <amandsvv4006@gmail.com>
2025-03-24 10:58:52 +0530

    frontend updated

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,12 +3,18 @@ import nodemailer from 'nodemailer';
 const sendEmail = async (to, subject, html) => {
     try {
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 465, // Try 465
+            secure: true,
             auth: {
-                user: process.env.EMAIL_USER, // Your email
-                pass: process.env.EMAIL_PASS, // Your email password or app password
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
             },
+            logger: true,
+            debug: true,
         });
+        
+        
 
         const mailOptions = {
             from: process.env.EMAIL_USER,
