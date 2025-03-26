73051f159fdb296b1f280f73cd5b1e172119f812
Aman Kumar <amandsvv4006@gmail.com>
2025-03-26 13:57:57 +0530

    frontend approved

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,18 +1,19 @@
 import express from "express";
 import { createSPHead, deleteSPHead, getSPHeadDetails, getSPHeads, updateSPHead } from "../controllers/admin.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { authorizeRoles } from "../middlewares/authorizeRole.middleware.js";
 
 const router = express.Router();
 
-router.route("/dashboard").get( verifyJWT, (req, res) => {
+router.route("/dashboard").get( verifyJWT,authorizeRoles("Admin"), (req, res) => {
     const user = req.user;
     res.render("adminDashboard",{user});
 });
 
-router.route("/spheads").get( verifyJWT,getSPHeadDetails);
-router.route("/sphead-control").get(verifyJWT,getSPHeads);       // Get all SPHeads
-router.route("/spheads/add").post(verifyJWT,createSPHead);    // Create a new SPHead
-router.route("/spheads/update/:id").put(verifyJWT, updateSPHead); // Update an SPHead
-router.route("/spheads/delete/:id").delete(verifyJWT, deleteSPHead); // Delete an SPHead
+router.route("/spheads").get( verifyJWT,authorizeRoles("Admin"),getSPHeadDetails);
+router.route("/sphead-control").get(verifyJWT,authorizeRoles("Admin"),getSPHeads);       // Get all SPHeads
+router.route("/spheads/add").post(verifyJWT,authorizeRoles("Admin"),createSPHead);    // Create a new SPHead
+router.route("/spheads/update/:id").put(verifyJWT, authorizeRoles("Admin"),updateSPHead); // Update an SPHead
+router.route("/spheads/delete/:id").delete(verifyJWT,authorizeRoles("Admin"), deleteSPHead); // Delete an SPHead
 
 export default router;
--- src/routes/superAdmin.routes.js
+++ src/routes/superAdmin.routes.js
@@ -1,20 +1,21 @@
 import express from "express";
 import { verifyJWT } from '../middlewares/auth.middleware.js';
+import { authorizeRoles } from '../middlewares/authorizeRole.middleware.js';
 import { addAdmin, deleteAdmin, getAdmin, getAllAdmins, superAdminDashboard, updateAdmin } from "../controllers/superAdmin.controllers.js";
 
 const router = express.Router();
 
-router.route('/dashboard').get(verifyJWT,superAdminDashboard)
+router.route('/dashboard').get(verifyJWT,authorizeRoles("SuperAdmin"),superAdminDashboard)
 // ✅ Fetch all Admins
-router.route("/admins").get(verifyJWT,getAllAdmins)
+router.route("/admins").get(verifyJWT,authorizeRoles("SuperAdmin"),getAllAdmins)
 //fetch a single admin
-router.route("/admins/:id").get(verifyJWT,getAdmin)
+router.route("/admins/:id").get(verifyJWT,authorizeRoles("SuperAdmin"),getAdmin)
 // ✅ Add a new Admin
-router.route("/admins/add").post(verifyJWT,addAdmin)
+router.route("/admins/add").post(verifyJWT,authorizeRoles("SuperAdmin"),addAdmin)
 // ✅ Update Admin
-router.route("/admins/update/:id").put(verifyJWT,updateAdmin)
+router.route("/admins/update/:id").put(verifyJWT,authorizeRoles("SuperAdmin"),updateAdmin)
 // ✅ Delete Admin
-router.route("/admins/delete/:id").delete(verifyJWT,deleteAdmin)
+router.route("/admins/delete/:id").delete(verifyJWT,authorizeRoles("SuperAdmin"),deleteAdmin)
 
 
 
