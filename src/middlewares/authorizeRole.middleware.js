ff8b5f415eef763ee508b464477d60c12ecffe06
Aman Kumar <amandsvv4006@gmail.com>
2025-03-31 00:03:37 +0530

    updated authorizeRoles

--- src/middlewares/authorizeRole.middleware.js
+++ src/middlewares/authorizeRole.middleware.js
@@ -1,7 +1,7 @@
 export const authorizeRoles = (...roles) => {
     return (req, res, next) => {
         if (!roles.includes(req.user.role)) {
-            return res.status(403).render("errorPage", { message: "Access Denied" });
+            return res.status(403).json({ success: false, message: "Access Denied" });
         }
         next();
     };
